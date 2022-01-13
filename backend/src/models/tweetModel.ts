2b391d41596c1914aa1ad79a93102daf9e89edeb
yasser570 <1007yasser@gmail.com>
2022-01-13 22:50:52 +0200

    create mongoose models/schema

---
+++ backend/src/models/tweetModel.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import { IUser } from "./userModel";
+
+const Schema = mongoose.Schema;
+
+export interface ITweet {
+  user: IUser;
+  body: string;
+  created: any;
+}
+
+const TweetSchema = new Schema<ITweet>({
+  user: { type: Schema.Types.ObjectId, ref: "User" },
+  body: { type: String, maxlength: 280 },
+  created: { type: Date, default: Date.now() },
+});
+
+export default mongoose.model<ITweet>("Tweet", TweetSchema);
---
+++ backend/src/models/userModel.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+import { ITweet } from "./tweetModel";
+
+const Schema = mongoose.Schema;
+
+export interface IUser {
+  name: string;
+  username: string;
+  password: string;
+  tweets: ITweet[];
+  created: any;
+}
+
+const UserSchema = new Schema<IUser>({
+  name: { type: String, maxlength: 50, minlength: 4 },
+  username: {
+    type: String,
+    maxlength: 15,
+    minlength: [4, "username must be at least 4 characters"],
+    unique: true,
+  },
+  password: { type: String, minlength: 4 },
+  tweets: [{ type: Schema.Types.ObjectId, ref: "Tweet" }],
+  created: { type: Date, default: Date.now() },
+});
+
+export default mongoose.model<IUser>("User", UserSchema);
